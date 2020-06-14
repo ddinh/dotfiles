6ae26e13f4213052dc56c58aab4486b77d805c4d
ddinh <ddinh@users.noreply.github.com>
2020-06-14 17:27:26 -0500

    Add zsh config

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,3 +4,8 @@ sudo apt update
 sudo apt install software-properties-common
 sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt install -y ansible
+
+ansible-playbook -i hosts dotfiles.yml
+
+git config --global user.name didnh
+git config --global user.email ddinh@users.noreply.github.com
