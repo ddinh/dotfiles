8c67db57ea4df9b1c973e2e7016f33a428248b29
ddinh <ddinh@users.noreply.github.com>
2020-06-15 16:41:28 -0500

    Fix permissions and add build-essential installation

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@
 sudo apt update
 sudo apt install software-properties-common
 sudo apt-add-repository --yes --update ppa:ansible/ansible
-sudo apt install -y ansible
+sudo apt install -y ansible build-essential
 
 # Install git if needed
 if ! [ -x "$(command -v git)" ]; then
