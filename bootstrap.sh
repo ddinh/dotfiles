98747ea35e18d851275607466da4d0d8709770cd
ddinh <ddinh@users.noreply.github.com>
2020-06-15 15:52:01 -0500

    Add homebrew configuration

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,11 @@ sudo apt install software-properties-common
 sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt install -y ansible
 
-ansible-playbook -i hosts dotfiles.yml
+source <(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)
+
+eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+
+ansible-playbook -i ~/dotfiles/hosts ~/dotfiles/dotfiles.yml
 
 git config --global user.name didnh
 git config --global user.email ddinh@users.noreply.github.com
