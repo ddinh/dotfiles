f7707d285504743cffbe405ac7304845f01d389a
ddinh <ddinh@users.noreply.github.com>
2020-06-15 16:18:54 -0500

    Add repo cloning to bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,6 +6,15 @@ sudo apt install software-properties-common
 sudo apt-add-repository --yes --update ppa:ansible/ansible
 sudo apt install -y ansible
 
+# Install git if needed
+if ! [ -x "$(command -v git)" ]; then
+	sudo apt install -y git
+fi
+
+# Clone the dotfiles repo
+git clone git@github.com:ddinh/dotfiles.git ~/dotfiles
+cd ~/dotfiles
+
 # Install and configure brew
 source <(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)
 eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
