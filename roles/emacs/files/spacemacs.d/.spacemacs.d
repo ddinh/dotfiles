fd3d15ba287c39e6bceb1c9379dc6df300ca171d
ddinh <ddinh@users.noreply.github.com>
2020-06-16 00:02:19 -0500

    Add emacs configuration

---
+++ roles/emacs/files/spacemacs.d/.spacemacs.d
@@ -0,0 +1,1 @@
+/home/ia/.spacemacs.d
---
+++ roles/emacs/files/spacemacs.d/theme.el
@@ -0,0 +1,22 @@
+(require 'doom-themes)
+
+;; Global settings (defaults)
+(setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+      doom-themes-enable-italic t) ; if nil, italics is universally disabled
+
+;; Load the theme (doom-one, doom-molokai, etc); keep in mind that each theme
+;; may have their own settings.
+(load-theme 'doom-city-lights t)
+
+;; Enable flashing mode-line on errors
+(doom-themes-visual-bell-config)
+
+;; Corrects (and improves) org-mode's native fontification.
+(doom-themes-org-config)
+
+;; Change powerline separator to arrow
+(setq powerline-default-separator 'arrow)
+(spaceline-compile)
+
+;; Increase line spacing
+(setq-default line-spacing 0.3)
